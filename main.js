2c2c22f448ddf8480c6be1b69ad9b2e2de7ae7bb
Charkrid 2009 <charkrid@gmail.com>
2023-05-13 18:11:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,58 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Add Movie</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<body>
-  <form id="add-movie-form">
-    <label for="imdb-url">IMDB URL:</label><br>
-    <input type="text" id="imdb-url" name="imdb-url"><br>
-    <input type="submit" value="Submit">
-  </form>
+const express = require('express');
+const { Client } = require('@notionhq/client');
 
-  <script>
-    $('#add-movie-form').on('submit', function(event) {
-      event.preventDefault();
+const app = express();
+app.use(express.json());
 
-      const imdbUrl = $('#imdb-url').val();
+// Initialize Notion client
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-      $.post('/add-movie', { imdbUrl }, function(data) {
-        alert(data.message);
-      });
+// Endpoint to add a movie to the database
+app.post('/add-movie', async (req, res) => {
+  try {
+    const { imdbUrl } = req.body;
+    // TODO: Fetch movie details from IMDB using the given URL
+
+    // Add a new page to the Notion database
+    await notion.pages.create({
+      parent
+      parent: { database_id: process.env.NOTION_DATABASE_ID },
+      properties: {
+        // TODO: Set these properties based on the fetched movie details
+        Name: { 
+          title: [
+            {
+              text: {
+                content: 'Movie Name',
+              },
+            },
+          ],
+        },
+        Watched: { 
+          checkbox: false,
+        },
+        Genres: {
+          multi_select: [
+            {
+              name: 'Genre1',
+            },
+            {
+              name: 'Genre2',
+            },
+            // Add more genres as needed
+          ],
+        },
+        // Add more properties as needed
+      },
     });
-  </script>
-</body>
-</html>
+
+    res.status(200).send({ message: 'Movie added successfully.' });
+  } catch (error) {
+    res.status(500).send({ message: 'An error occurred.' });
+  }
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running.');
+});
